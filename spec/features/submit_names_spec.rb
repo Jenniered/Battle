14f3052348099948dee69d86d22ff5b2cc1562c2
Jennie Evans <jennie1evans@hotmail.com>
2022-06-01 17:20:31 +0100

    attacking player 2

--- spec/features/submit_names_spec.rb
+++ spec/features/submit_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'Players submit names in a form and see names on-screen' do
-    visit('/')
-    fill_in :player_1_name, with: 'Charlotte'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
+    sign_in_and_play
 
     #save_and_open_page
 
