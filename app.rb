e2d27755a3805b27169979b316d6ba74e2e1d144
Jennie Evans <jennie1evans@hotmail.com>
2022-06-07 09:57:19 +0100

    implementing hit points

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require './lib/player'
+require './lib/game'
 
 class Battle < Sinatra::Base
   configure :development do
@@ -19,14 +21,16 @@ class Battle < Sinatra::Base
   end
   
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1 = $player_1
+    @player_2 = $player_2
     erb :play
   end
   
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1 = $player_1
+    @player_2 = $player_2
+    #@player_1.attack(@player_2)
+    Game.new.attack(@player_2)
     erb :attack
   end
 
